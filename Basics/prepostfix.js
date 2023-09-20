01c9ba78aa8e4d524dfbb3d94af8ba12170ae26a
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-09-20 12:05:43 +0530

    summary

---
+++ Basics/prepostfix.js
@@ -0,0 +1,17 @@
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+                                 //Prefix
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+
+
+let a = 7;
+const b = a++;
+// x is 4; y is 4
+                          // postfix
+let a2 = 4n;
+const b2 = a2++;
+// x2 is 4n; y2 is 4n
