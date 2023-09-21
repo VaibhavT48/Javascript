6542456801acfacf82e0d0c8ecd671c4aa6afcbc
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-09-21 11:24:21 +0530

    num

---
+++ Basics/numandmath.js
@@ -0,0 +1,35 @@
+
+//++++++++Number+++++++
+
+
+const score = 300
+console.log(score);
+
+
+const balance  = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const anotherNumber = 241.7594
+
+console.log(anotherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++Math++++++++++++++++++++++++++++++++++++
+console.log(Math.round(3.7));
+console.log(Math.ceil(3.7));
+console.log(Math.floor(3.7));
+console.log(Math.min(3,3,6,7,8));
+console.log(Math.max(23,45,32,23));
+
+const min = 10
+const max = 20
+
+console.log(math.floor(math.random()*(max - min + 1)) +min);
+
+
