84efbf633edee32e7571f1935a54c502f1148ca0
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-09-29 13:16:46 +0530

    api

---
+++ Basics01/destjason.js
@@ -0,0 +1,24 @@
+const course = {
+
+    coursename: "js in hindi",
+    price: "74786",
+    courseintructor: "hitesh"
+
+}
+
+const {courseintructor:instructor} = course
+console.log(instructor);
+
+
+
+//API
+
+
+{
+    name = "vaibhav"
+}
+[
+    {
+        name = "vaibhav"
+    }
+]
