df47524015bd57fee1ff178f77e4a82cccfc4c6e
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-09-30 14:50:18 +0530

    function with object

---
+++ Basics02/funobj.js
@@ -0,0 +1,43 @@
+function calculateCartPrice(...num1){   /// rest operator
+return num1
+}
+
+console.log(calculateCartPrice(100,200,300));
+
+
+ const user = {
+    user:"vaibhav",
+    price:7372
+ }                                       ///  object
+
+ function handleObject(anyObject){
+    console.log(`user name is ${anyObject.user} and price is ${anyObject.price}`);
+ }
+
+ handleObject(user)
+
+
+
+
+
+ handleObject({
+    username:"mark",     /// object
+    id:8928392
+ })
+
+
+
+ const myNewArray = [100,2443,1243]
+
+ function getSecondNumber(getArray){
+ return getArray[1]
+ }                                              /// array
+
+ console.log(getSecondNumber(myNewArray));
+
+
+
+
+
+
+
