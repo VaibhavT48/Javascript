5373189ad75da2cbdccb8e181815d96646a2f2b8
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-09-30 11:35:22 +0530

    also para

---
+++ Basics02/function.js
@@ -0,0 +1,44 @@
+
+function sayMyName(){
+
+    console.log("v");
+    console.log("A");
+    console.log("I");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("v");
+}
+
+sayMyName()
+
+function addTwoNumber(number1, number2){
+
+let result = number1 + number2
+return number1 + number2
+}                                               // return 
+
+addTwoNumber(54, 87)
+
+const result = addTwoNumber(6,8)
+console.log("result: ", result);
+
+
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+
+    console.log(loginUserMessage("vaibhav"))
+}
+
+
+function loginUserMessage(username){
+
+    if(username === undefined){
+
+        console.log("please enter your username");
+        return
+    }
+}
+
