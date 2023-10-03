829d3f6efcc249ea1c7b28a50c3af2137de051cd
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-10-03 15:31:26 +0530

    values

---
+++ controlflow/truthy.js
@@ -0,0 +1,17 @@
+const userEmail = "vaibhav@gmail.com"
+
+if (userEmail) {
+    console.log("got userEmail");
+}
+
+else{
+    console.log("not found");
+}
+
+//// falsy values
+
+// false , 0 , -0, BigInt 0n, "", null, undefined
+
+// truthy values
+
+// 0, 'false', " ", [], {}, function(){}
