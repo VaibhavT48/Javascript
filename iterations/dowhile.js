27b6e690e994fd3a163545a5dfd28399663afb8e
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-10-04 11:27:36 +0530

    while

---
+++ iterations/dowhile.js
@@ -0,0 +1,18 @@
+let index = 0
+
+while (index <= 20) {
+    console.log(`value of index is${index}`);
+    index = index + 2
+    
+}
+ //  while
+
+ let myArray = ["one", "two", "three"]
+
+ let arr = 0 
+
+ while (arr< myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+ }
