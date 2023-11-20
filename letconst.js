ae7942c033109fdb9b6729ce43d6025d12dd2b83
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-11-20 14:30:42 +0530

    pull

--- letconst.js
+++
@@ -1,9 +0,0 @@
-const accountId = "67648773"
-console.log(accountId);
-
-//modification not allowed 
-
-let userEmail = "vaibhavyadav4884@mail.com"
-console.log(userEmail);
-
-//modification allowed
--- stringnumber.js
+++
@@ -1,6 +0,0 @@
-let str1 = "hello"
-
-let str2 = " vaibhav "
-
-let str3 = str1 + str2
-console.log(str3);
