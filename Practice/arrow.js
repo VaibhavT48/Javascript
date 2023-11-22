b47eb6564073b6b37afd5afcce1681cc4ec1cb92
Vaibhav Vaijnath Yadav <vaobhavyadav4884@gmail.com>
2023-11-22 12:44:32 +0530

    conditions

---
+++ Practice/arrow.js
@@ -0,0 +1,42 @@
+const arrow = (num1,  num2)=>{
+    return num1 + num2
+}
+
+console.log(arrow(45,665));
+
+// if else
+
+const temperature = 46
+
+if (temperature >50) {
+
+console.log("temperature is less than 50");
+    
+}
+
+else{
+    console.log("more than 50");
+}
+
+//  switch
+
+const month = 3
+
+switch (month) {
+    case 1: "January"
+    console.log("January");
+    break;
+
+    case 2: "February"
+    console.log("feb");
+    break;
+    
+    case 3: "April"
+    console.log("april");
+    break
+
+    default:
+        console.log("not month found");
+        break;
+}
+
---
+++ Practice/higherorder.js
@@ -0,0 +1,45 @@
+const code = ["ruby", "java", "c++", "javascript"]
+
+code.forEach(function(code){
+    console.log(code);
+})
+
+
+// By using arrow finction
+
+code.forEach((code) => {
+    console.log(code);
+})
+
+
+//filter
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+ const newnums = mynums.filter((num) =>{
+  return num>8
+})
+
+console.log(newnums);
+
+// map
+
+const newnum = mynums.map((num)=> num+10)
+console.log(newnum);
+
+// reduse
+
+const mynnumber = [1,2,3,4,5,6,7,8]
+myTotal = mynnumber.reduce(function(acc,curval){
+    return acc + curval
+},0)
+
+//console.log(myTotal);
+
+// by using arrow function
+
+myTotal = mynnumber.reduce((acc, curval )=> acc + curval,0)
+console.log(myTotal);
+
+
+
